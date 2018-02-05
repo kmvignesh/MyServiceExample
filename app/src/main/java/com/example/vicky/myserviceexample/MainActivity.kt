5fb4c37d72d628d0b4d9a41c59f06566f3a1e7bf
VickY <kmvignesh94@gmail.com>
2018-02-05 09:24:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vicky/myserviceexample/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.vicky.myserviceexample
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val intent = Intent(this,MyService::class.java)
+        startService(intent)
+
+    }
+}
